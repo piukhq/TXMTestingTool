8093ae24fd78991d428e02d8675c0491a4c2d693
Chris Latham <backwardspy@gmail.com>
2020-04-01 12:22:53 +0100

    add "generate output" view controller and fix layout issues

---
+++ TXMTestingTool/Scenes/GenerateOutputViewController.swift
@@ -0,0 +1,26 @@
+//
+//  GenerateOutputViewController.swift
+//  TXMTestingTool
+//
+//  Created by Chris Latham on 01/04/2020.
+//  Copyright © 2020 Bink. All rights reserved.
+//
+
+import Cocoa
+
+class GenerateOutputViewController: NSViewController {
+    // MARK: - Helpers
+
+    // MARK: - IBOutlets
+
+    // MARK: - Properties
+
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - IBActions
+
+    // MARK: - General
+}
