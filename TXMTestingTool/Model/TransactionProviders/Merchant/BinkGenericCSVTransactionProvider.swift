59f13d698353aa5a57d52b751bc14478b8fe4178
backwardspy <backwardspy@pigeon.life>
2023-07-03 14:40:09 +0100

    feat: add generic bink csv transaction provider for itsu

---
+++ TXMTestingTool/Model/TransactionProviders/Merchant/BinkGenericCSVTransactionProvider.swift
@@ -0,0 +1,56 @@
+//
+//  BinkGenericSFTPTransactionProvider.swift
+//  TXMTestingTool
+//
+//  Created by Chris Latham on 03/07/2023.
+//  Copyright © 2023 Bink. All rights reserved.
+//
+
+import Foundation
+import CSV
+
+struct BinkGenericCSVTransactionProvider: CSVProvider {
+
+    // MARK: - Properties
+
+    var defaultFileName = "transactions.csv"
+
+    var delimiter = ","
+
+    var columnHeadings = [
+        "transaction_id",
+        "payment_card_type",
+        "payment_card_first_six",
+        "payment_card_last_four",
+        "amount",
+        "currency_code",
+        "auth_code",
+        "date",
+        "merchant_identifier",
+        "retailer_location_id",
+        "transaction_data",
+        "customer_id",
+    ]
+
+    private let dateFormatter = ISO8601DateFormatter()
+
+    // MARK: - Supporting Functions
+
+    func transactionToColumns(_ transaction: Transaction, merchant: MerchantAgent, paymentProvider: PaymentAgent, sequenceNumber: Int) throws -> [String] {
+        [
+            transaction.id,
+            paymentProvider.slug,
+            transaction.firstSix,
+            transaction.lastFour,
+            "\(Double(transaction.amount) / 100)",
+            "GBP",
+            transaction.authCode,
+            dateFormatter.string(from: transaction.date),
+            transaction.mid,
+            "86",  // location ID
+            "",  // transaction data
+            "",  // customer ID
+        ]
+    }
+
+}
